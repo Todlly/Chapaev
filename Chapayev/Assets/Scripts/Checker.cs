94957ae8d6cbb3530c71c50c6f5f83c996e313bd
Sergey <furrykunstler@gmail.com>
2019-09-30 20:48:02 +0300

    Added checker selector and hitter

---
+++ Chapayev/Assets/Scripts/Checker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checker : MonoBehaviour
+{
+    public Material highlightedMat;
+    public Material defaultMat;
+    private Renderer rend;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    public void Highlight()
+    {
+        rend.material = highlightedMat;
+    }
+
+    public void DeHighlight()
+    {
+        rend.material = defaultMat;
+    }
+
+    public void Hit()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * 300);
+    }
+}
---
+++ Chapayev/Assets/Scripts/HighlightingRay.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightingRay : MonoBehaviour
+{
+    Camera cam;
+    int layerCheckers, layerBoard;
+    private Checker selChecker;
+    private bool selected = false, holded = false;
+    private Vector3 direction;
+    void Start()
+    {
+        layerCheckers = LayerMask.GetMask("Checkers");
+        layerBoard = LayerMask.GetMask("Board");
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Ray bRay = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerCheckers))
+        {
+            if (!selected)
+            {
+                selChecker = hit.transform.gameObject.GetComponent<Checker>();
+                selChecker.Highlight();
+                selected = true;
+            }
+        }
+        else if (selected)
+        {
+            if (!holded)
+            {
+                selChecker.DeHighlight();
+                selChecker = null;
+                selected = false;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (selected)
+            {
+                if (!holded)
+                {
+                    holded = true;
+                }
+                RaycastHit boardHit;
+                Physics.Raycast(bRay, out boardHit, Mathf.Infinity, layerBoard);
+                Debug.Log(direction.ToString());
+                Debug.Log("hitpos " + boardHit.point);
+                Vector3 from = new Vector3(boardHit.point.x, selChecker.transform.position.y, boardHit.point.z);
+                direction = selChecker.transform.position - from;
+            }
+        }
+        else if (holded)
+        {
+            selChecker.transform.gameObject.GetComponent<Rigidbody>().AddForce(direction * 300);
+            holded = false;
+        }
+    }
+}
